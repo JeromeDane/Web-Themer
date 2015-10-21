dbc15765c3f285bbe1123d00a7f5197bae997e7a
Jerome Dane <jerome.dane@gmail.com>
2015-10-20 21:06:18 -0400

    Save point

---
+++ gulp/webpack.js
@@ -0,0 +1,25 @@
+var webpackOrig = require('webpack');
+
+// run webpack, applying project specific settings
+module.exports = function(config, callback) {
+	config['dev-tools'] = 'source-map';
+	config.output = {
+		path: "./build",
+		filename: "[name].js"
+	};
+	config.module = config.module || {};
+	config.module.loaders = config.module.loaders || [];
+	config.module.loaders.push({
+		test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+	});
+	config.module.loaders.push(
+		{ test: /\.html$/, loader: "underscore-template-loader" }
+	);
+	/*
+	config.module.loaders.push(
+		//{test: /\.png$/, loader: "url-loader?mimetype=image/png"},
+	);
+	*/
+	webpackOrig(config, callback);
+};
--- gulpfile.js
+++ gulpfile.js
@@ -4,41 +4,16 @@ var template = require('gulp-template');
 var fs = require('fs');
 var io = require('socket.io');
 var watch = require('gulp-watch');
-var webpackStream = require('webpack-stream');
-var webpack = require('webpack');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var webpack = require('./gulp/webpack');
 
 // web socket port for chrome auto-reload extension (https://github.com/JeromeDane/chrome-extension-auto-reload)
 var WEB_SOCKET_PORT = 8890;
 
 function getPackageDetails() {
 	return JSON.parse(fs.readFileSync("./package.json", "utf8"));
-};
-
-function webpackExtend(config, callback) {
-	
-	config['dev-tools'] = 'source-map';
-	config.output = {
-		path: "./build",
-		filename: "[name].js"
-	};
-	config.module = config.module || {};
-	config.module.loaders = config.module.loaders || [];
-	config.module.loaders.push({
-		test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
-	});
-	config.module.loaders.push(
-		{ test: /\.html$/, loader: "underscore-template-loader" }
-	);
-	/*
-	config.module.loaders.push(
-		//{test: /\.png$/, loader: "url-loader?mimetype=image/png"},
-	);
-	*/
-	webpack(config, callback);
 };
 
 // remove all build and distribution files
@@ -72,19 +47,7 @@ gulp.task('manifest', function () {
 			.pipe(gulp.dest('build'));
 });
 
-gulp.task('background', function () {
-	return gulp.src("./src/background.js")
-			.pipe(webpack({
-				module: {
-					loaders: [
-						//{test: /\.png$/, loader: "url-loader?mimetype=image/png"},
-						//{test: /\.css$/, loader: 'style!css' }
-					]
-				},
-				devtool: 'inline-source-map'
-			}))
-			.pipe(rename("background.js"))
-			.pipe(gulp.dest('./build'));
+gulp.task('background', ['background-js', 'background-html'], function () {
 });
 
 gulp.task('options', ['options-js', 'options-html'], function (callback) {
@@ -96,18 +59,13 @@ gulp.task('options-html', function () {
 			.pipe(gulp.dest('build'));
 });
 
-gulp.task('scripts', function (callback) {
+gulp.task('min', function (callback) {
 	webpack({
 		entry: {
 			options: "./src/options.js",
 			background: "./src/background.js",
 			content: "./src/content.js"
 		},
-		output: {
-			path: "./build",
-			filename: "[name].js"
-		},
-		devtool: 'source-map',
 		plugins: [
 			new webpack.optimize.DedupePlugin(),
 			new webpack.optimize.UglifyJsPlugin({
@@ -120,31 +78,19 @@ gulp.task('scripts', function (callback) {
 });
 
 gulp.task('options-js', function (callback) {
-	webpackExtend({
+	webpack({
 		entry: {
 			options: "./src/options.js"
 		}
 	}, callback);
 });
 
-gulp.task('content', function () {
-	return gulp.src("./src/content.js")
-			.pipe(webpack({
-				module: {
-					loaders: [
-						//{test: /\.png$/, loader: "url-loader?mimetype=image/png"},
-						//{test: /\.css$/, loader: 'style!css' }
-					]
-				},
-				devtool: 'inline-source-map',
-				resolve: {
-					alias: {
-						//		'app/options': './src/com/jeromedane/webthemer/options'
-					}
-				}
-			}))
-			.pipe(rename("content.js"))
-			.pipe(gulp.dest('./build'));
+gulp.task('content', function (callback) {
+	webpack({
+		entry: {
+			options: "./src/content.js"
+		}
+	}, callback);
 });
 
 gulp.task('watch', ['default', 'watch-reloader', 'watch-options'], function () {
